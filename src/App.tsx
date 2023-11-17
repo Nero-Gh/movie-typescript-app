0df34e1ed96c116206b9e051823ed6207d494a3d
Bismark Kadogbe <67119909+Nero-Gh@users.noreply.github.com>
2023-11-17 15:42:46 +0000

    added routes to the App directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MovieDetailPage from "./pages/MovieDetailPage";
 
 function App() {
   return (
     <>
-      <div>Nero</div>
+      <div className="w-5/6 mx-auto my-16 ">
+        <Navbar />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/:id" element={<MovieDetailPage />} />
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
